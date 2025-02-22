2b2fdf6937dcca71a10c895eb99f643d800787c5
Hubert Stanowski <hubert.stanowski@gmail.com>
2025-02-22 17:29:27 +0000

    Fix migrations

--- backend/pub/views.py
+++ backend/pub/views.py
@@ -2,8 +2,9 @@ from rest_framework.decorators import api_view, renderer_classes
 from rest_framework.renderers import JSONRenderer, BrowsableAPIRenderer
 from rest_framework.response import Response
 
-from backend.tab.views import Tab
+from tab.views import Tab
 from .models import Pub
+
 
 @api_view(['GET'])
 @renderer_classes([JSONRenderer, BrowsableAPIRenderer])
@@ -15,6 +16,7 @@ def pub_list(request):
     pubs = Pub.objects.all()
     pubs_list = list(pubs.values())
     return Response(pubs_list)
+
 
 def busy_percentage():
     """
