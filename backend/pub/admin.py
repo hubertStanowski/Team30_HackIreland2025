674fa4799483101813fbbc0b6ca90f5e1ae9d8c7
timothytay <timotheostay@gmail.com>
2025-02-23 10:06:08 +0000

    Make FavoriteDrink an admin site accessible table

--- backend/pub/admin.py
+++ backend/pub/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Pub, Table, Drink, DrinkType
+from .models import Pub, Table, Drink, DrinkType, FavoriteDrink
 
 # Register your models here.
 
@@ -7,3 +7,4 @@ admin.site.register(Pub)
 admin.site.register(Table)
 admin.site.register(Drink)
 admin.site.register(DrinkType)
+admin.site.register(FavoriteDrink)
