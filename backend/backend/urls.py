f49b0de52696b1df14bf7431d14e80988260bdcb
timothytay <timotheostay@gmail.com>
2025-02-23 08:13:10 +0000

    Setting update

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -17,6 +17,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from .views import login_view, logout_view, register_view, change_password
+from django.conf import settings
+from django.conf.urls.static import static
 
 
 urlpatterns = [
@@ -28,3 +30,6 @@ urlpatterns = [
     path('auth/register/', register_view, name='register'),
     path('auth/change_password/', change_password)
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
