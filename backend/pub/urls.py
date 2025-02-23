ddfb4c7d8aa2e20b008816a2ddc5c3fa0b7c81b6
timothytay <timotheostay@gmail.com>
2025-02-23 11:09:40 +0000

    AI drink recommender

--- backend/pub/urls.py
+++ backend/pub/urls.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.urls import path
-from .views import pub_list, pub_tabs, unpaid_pub_tabs, busy_percentage, pub_products
+from .views import pub_list, pub_tabs, unpaid_pub_tabs, busy_percentage, pub_products, add_favorite_drink, remove_favorite_drink, list_favorite_drinks_by_pub, get_drink_suggestion
 
 
 urlpatterns = [
@@ -8,5 +8,9 @@ urlpatterns = [
     path("tabs/", pub_tabs),
     path("unpaid_tabs/", unpaid_pub_tabs),
     path("busy/", busy_percentage),
-    path("items/", pub_products)
+    path("items/", pub_products),
+    path('favorites/add/', add_favorite_drink, name='add_favorite_drink'),
+    path('favorites/remove/', remove_favorite_drink, name='remove_favorite_drink'),
+    path('favorites/', list_favorite_drinks_by_pub, name='list_favorite_drinks_by_pub'),
+    path('drink_suggestion/', get_drink_suggestion)
 ]
